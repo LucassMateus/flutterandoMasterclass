342dd309f6c862528a527d415533f008e5512bbd
Lucas Mateus <spechtlucasmateus@gmail.com>
2023-04-12 21:20:09 -0300

    expressões regulares

--- FlutterandoRegex/desafio2.dart
+++ FlutterandoRegex/desafio2.dart
@@ -1,6 +1,6 @@
-main () {
+main() {
   final groupRegex = RegExp(r'(\d{3}-\d{3})');
-  final match= groupRegex.firstMatch('123-232 é seu código');
-  
+  final match = groupRegex.firstMatch('123-232 é seu código');
+
   print(match?.group(1));
 }
