3919b9566041ad95f199bd115535be7381223ea0
Lucas Mateus <spechtlucasmateus@gmail.com>
2023-04-21 10:00:13 -0300

    ajuste para não deixar o contador ficar negativo

--- GerenciamentoEstado/inherited_notifier/lib/shopping_cart_controller.dart
+++ GerenciamentoEstado/inherited_notifier/lib/shopping_cart_controller.dart
@@ -16,6 +16,6 @@ class ShoppingCartController extends InheritedNotifier<ValueNotifier<int>> {
   }
 
   reduce() {
-    notifier!.value--;
+   notifier!.value > 0 ? notifier!.value-- : notifier!.value;
   }
 }
